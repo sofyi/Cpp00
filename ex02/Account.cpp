445e7c766167f7d70a31755474503c086cac3df7
safia <angel@localhost.localdomain>
2025-11-18 17:26:42 +0100

    push

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -12,10 +12,10 @@
 
 #include "Account.hpp"
 
-//  int	Account::_nbAccounts = 0;
-//  int	Account::_totalAmount = 0;
-//  int	Account::_totalNbDeposits = 0;
-//  int	Account::_totalNbWithdrawals = 0; 
+ int	Account::_nbAccounts = 0;
+ int	Account::_totalAmount = 0;
+ int	Account::_totalNbDeposits = 0;
+ int	Account::_totalNbWithdrawals = 0; 
  
 int Account::getNbAccounts( void )
 {
--- ex02/Account.hpp
+++ ex02/Account.hpp
@@ -62,10 +62,7 @@ private:
 	Account( void );
 
 };
-int	Account::_nbAccounts = 0;
-int	Account::_totalAmount = 0;
-int	Account::_totalNbDeposits = 0;
-int	Account::_totalNbWithdrawals = 0; 
+
 
 
 
